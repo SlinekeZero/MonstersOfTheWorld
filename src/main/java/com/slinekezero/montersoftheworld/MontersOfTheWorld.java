8586f2a788867b8317f52859d6d31367fe61a14c
SlinekeZero <slinekezero@gmail.com>
2024-08-13 14:27:27 -0500

    Plantilla base mod}

---
+++ src/main/java/com/slinekezero/montersoftheworld/MontersOfTheWorld.java
@@ -0,0 +1,8 @@
+package com.slinekezero.montersoftheworld;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(MontersOfTheWorld.MODID)
+public class MontersOfTheWorld {
+    public static final String MODID = "montersoftheworld";
+}
